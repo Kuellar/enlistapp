44a587bd8f4ba75c9afab6047d45276dcc4703c4
Kuellar <icuellartapia@gmail.com>
2021-11-20 01:26:32 -0300

    Create profile

--- src/components/pages/Profile.js
+++ src/components/pages/Profile.js
@@ -5,8 +5,19 @@ import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import Avatar from "@mui/material/Avatar";
 import FotoPerfil from "../../images/FotoPerfil.png";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import PersonIcon from "@mui/icons-material/Person";
+import EmailIcon from "@mui/icons-material/Email";
+import QrCodeIcon from "@mui/icons-material/QrCode";
+import CheckIcon from "@mui/icons-material/Check";
 
 const Profile = () => {
   const theme = useTheme();
@@ -28,13 +39,132 @@ const Profile = () => {
         </Typography>
       </Box>
 
-      <Box sx={{ p: 5 }}>
-        <Card sx={{ maxWidth: 200 }}>
-          <CardMedia component="img" image={FotoPerfil} alt="Foto perfil" />
-          <CardContent>
-            <Typography variant="body1">@ignacio.cuellar</Typography>
-          </CardContent>
-        </Card>
+      <Box sx={{ p: { xs: 0, sm: 1, md: 2 } }}>
+        <Grid container>
+          <Grid
+            item
+            xs={12}
+            md={4}
+            sx={{
+              p: { xs: 1, sm: 2 },
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <Card
+              sx={{
+                width: "100%",
+                maxWidth: 300,
+                margin: "auto",
+              }}
+            >
+              <CardMedia
+                component="img"
+                image={FotoPerfil}
+                alt="Foto perfil"
+                sx={{ maxHeight: 300, maxWidth: 300 }}
+              />
+              <CardContent>
+                <Typography variant="body1" sx={{ textAlign: "center" }}>
+                  @ignacio.cuellar
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            md={8}
+            sx={{
+              p: { xs: 1, sm: 2 },
+              pr: { md: 2, lg: 4, xl: 6 },
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <Paper
+              sx={{
+                width: "100%",
+              }}
+            >
+              <List>
+                <ListItem>
+                  <ListItemAvatar>
+                    <Avatar
+                      sx={{ backgroundColor: theme.palette.primary.dark }}
+                    >
+                      <PersonIcon />
+                    </Avatar>
+                  </ListItemAvatar>
+                  <ListItemText
+                    primary="Nombre"
+                    secondary="Juan Rodriguez"
+                    secondaryTypographyProps={{
+                      color: theme.palette.primary.dark,
+                      paddingLeft: 2,
+                    }}
+                  />
+                </ListItem>
+
+                <ListItem>
+                  <ListItemAvatar>
+                    <Avatar
+                      sx={{ backgroundColor: theme.palette.primary.dark }}
+                    >
+                      <EmailIcon />
+                    </Avatar>
+                  </ListItemAvatar>
+                  <ListItemText
+                    primary="Mail"
+                    secondary="juan.rodriguez@gmail.com"
+                    secondaryTypographyProps={{
+                      color: theme.palette.primary.dark,
+                      paddingLeft: 2,
+                    }}
+                  />
+                </ListItem>
+
+                <ListItem>
+                  <ListItemAvatar>
+                    <Avatar
+                      sx={{ backgroundColor: theme.palette.primary.dark }}
+                    >
+                      <QrCodeIcon />
+                    </Avatar>
+                  </ListItemAvatar>
+                  <ListItemText
+                    primary="Pase de movilidad"
+                    secondary="20/11/2021"
+                    secondaryTypographyProps={{
+                      color: theme.palette.primary.dark,
+                      paddingLeft: 2,
+                    }}
+                  />
+                </ListItem>
+
+                <ListItem>
+                  <ListItemAvatar>
+                    <Avatar
+                      sx={{ backgroundColor: theme.palette.primary.dark }}
+                    >
+                      <CheckIcon />
+                    </Avatar>
+                  </ListItemAvatar>
+                  <ListItemText
+                    primary="Resultado"
+                    secondary="Negativo"
+                    secondaryTypographyProps={{
+                      color: theme.palette.primary.dark,
+                      paddingLeft: 2,
+                    }}
+                  />
+                </ListItem>
+              </List>
+            </Paper>
+          </Grid>
+        </Grid>
       </Box>
     </Box>
   );
