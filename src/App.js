536d500b4719a1b4dea6e4528b94626a6780c38a
Kuellar <icuellartapia@gmail.com>
2021-11-24 21:20:38 -0300

    Design for the landing page

--- src/App.js
+++ src/App.js
@@ -5,14 +5,37 @@ import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import TopbarLandingPage from "./components/TopbarLandingPage";
 import Footer from "./components/Footer";
+import Logo from "./images/logoLanding.png";
+import { TextLoop } from "react-text-loop-next";
 
 export default function App() {
   return (
     <Box>
       <Footer>
         <TopbarLandingPage />
-        <Container>
-          <Typography>Content...</Typography>
+        <Container sx={{ textAlign: "center" }}>
+          <Box
+            sx={{
+              width: { xs: "100%", sm: 400 },
+              ml: "auto",
+              mr: "auto",
+              pt: { xs: 10, sm: 20 },
+            }}
+          >
+            <img
+              src={Logo}
+              alt="Logo EnlistApp"
+              style={{ width: "100%", height: "100%" }}
+            />
+          </Box>
+          <Typography variant="body1" align="center" sx={{ fontSize: 30 }}>
+            Reunirse con los amigos nunca ha sido tan{" "}
+            <TextLoop springConfig={{ stiffness: 180, damping: 12 }}>
+              <span style={{ color: "#455a64" }}>fácil</span>
+              <span style={{ color: "#455a64" }}>rápido</span>
+              <span style={{ color: "#455a64" }}>seguro</span>
+            </TextLoop>
+          </Typography>
         </Container>
       </Footer>
     </Box>
